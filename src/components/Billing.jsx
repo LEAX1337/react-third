f787b1bb8092cb9a7a2aedec8af3cdf79ad0d9bf
NeverMasya <inf3rnalcr0w@gmail.com>
2023-08-21 01:47:19 +0300

    Done

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { apple, bill, google } from '../assets'
+import styles, {layout} from '../styles'
 
 const Billing = () => {
   return (
-    <div>Billing</div>
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img src={bill} alt="Billing" className='w-full h-full relative z-[5]' />
+        <div className='absolute z-[3] -left-1/2 top-0 w-1/2 h-1/2 rounded-full white__gradient'/>
+        <div className='absolute z-[0] -left-1/2 bottom-0 w-1/2 h-1/2 rounded-full pink__gradient'/>
+      </div>
+
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Easily controll your <br className='sm:block hidden' /> billing & invoicing.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur excepturi fugit explicabo possimus iste quis hic aperiam itaque sed dignissimos!</p>
+        <div className='flex flex-row flex-wrap sm:mt-10 my-6'>
+          <img src={apple} alt="Apple" className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer' />
+          <img src={google} alt="Google" className='w-[128px] h-[42px] object-contain cursor-pointer' />
+        </div>
+      </div>
+    </section>
   )
 }
 
